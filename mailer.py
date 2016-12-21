5d2aa369855997d0d102942841df59ff15a79ad5
ericzuobin <ericzuobin@qq.com>
2016-12-21 15:55:08 +0800

    增加IP和掩码计算，过滤白名单

--- mailer.py
+++ mailer.py
@@ -3,11 +3,13 @@
 
 import os
 import smtplib
+import socket
 import sys
 import json
 import logging
 from smtpd import COMMASPACE
 
+import struct
 from flask import request
 from email.mime.text import MIMEText
 
@@ -42,16 +44,66 @@ def configure_logging(level):
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
+
+def format_subnet(subnet_input):
+    # 如果输入的ip，将掩码加上后输出
+    if subnet_input.find("/") == -1:
+        return subnet_input + "/255.255.255.255"
+    else:
+        # 如果输入的是短掩码，则转换为长掩码
+        subnet = subnet_input.split("/")
+        if len(subnet[1]) < 3:
+            mask_num = int(subnet[1])
+            last_mask_num = mask_num % 8
+            last_mask_str = ""
+            for i in range(last_mask_num):
+                last_mask_str += "1"
+            if len(last_mask_str) < 8:
+                for i in range(8 - len(last_mask_str)):
+                    last_mask_str += "0"
+            last_mask_str = str(int(last_mask_str, 2))
+            if mask_num / 8 == 0:
+                subnet = subnet[0] + "/" + last_mask_str + "0.0.0"
+            elif mask_num / 8 == 1:
+                subnet = subnet[0] + "/255." + last_mask_str + ".0.0"
+            elif mask_num / 8 == 2:
+                subnet = subnet[0] + "/255.255." + last_mask_str + ".0"
+            elif mask_num / 8 == 3:
+                subnet = subnet[0] + "/255.255.255." + last_mask_str
+            elif mask_num / 8 == 4:
+                subnet = subnet[0] + "/255.255.255.255"
+            subnet_input = subnet
+            # 计算出正确的子网地址并输出
+        subnet_array = subnet_input.split("/")
+        subnet_true = socket.inet_ntoa(struct.pack("!I", struct.unpack("!I", socket.inet_aton(subnet_array[0]))[0] &
+                                                   struct.unpack("!I", socket.inet_aton(subnet_array[1]))[0])) + "/" + subnet_array[1]
+        return subnet_true
+
+white_ip_list = []
+subnet_white_array = []
+
+
+def config_white_list(white_list):
+    for white_ip in white_list:
+        temp_ip = format_subnet(white_ip)
+        white_ip_list.append(temp_ip)
+        subnet_white_array.append(temp_ip.split("/")[1])
+
+
 logger = logging.getLogger(__name__)
 configure_logging(logging.ERROR)
 
 config = load_config()
+config_white_list(config['whiteList'])
 
 
 @app.route('/')
 def init():
     logger.error("IP come %s", request.remote_addr)
-    return 'Hello Mailer!'
+    if check_ip(request.remote_addr):
+        return 'Hello Mailer!'
+    else:
+        return '500'
 
 
 @app.route('/sendMail', methods=['POST'])
@@ -63,7 +115,7 @@ def send_mail():
         status_code = 200
     except Exception, e:
         logger.error('发送邮件出错,%s', e.message)
-    return 'send', status_code
+    return status_code
 
 
 def smtp_send(smtp_config, m):
@@ -80,6 +132,13 @@ def smtp_send(smtp_config, m):
     s.login(smtp_config['username'], smtp_config['password'])
     s.sendmail(from_email, m['to'], msg.as_string())
     s.quit()
+
+
+def check_ip(r_ip):
+    for subnet in subnet_white_array:
+        if format_subnet(r_ip + "/" + subnet) in white_ip_list:
+            return True
+    return False
 
 
 if __name__ == '__main__':
